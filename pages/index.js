aaea3ac9430febcce851a7dfbe69155e158685d2
Andrii Pereverziev <andriipereverziev@Andriis-MacBook-Pro.local>
2020-11-13 08:18:35 +0200

    Create pages

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,8 @@
+import Navbar from "../navbar"
+
 const Home = () => (
   <div className="portfolio-app">
-    {/* NAVBAR START */}
-    <div className="navbar-wrapper">
-      <nav className="navbar navbar-expand-lg navbar-dark fj-mw9">
-        <a className="navbar-brand mr-3 font-weight-bold" href="#">FilipJerga</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav">
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">Portfolio</a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">Courses</a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">Cv</a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">Ask me</a>
-            </li>
-          </ul>
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">Sign Up</a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link btn btn-success bg-green-2 bright" href="#">Sign In</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-    {/* NAVBAR ENDS */}
+    <Navbar />
     {/* HERO STARTS */}
     <section className="fj-hero">
       <div className="fj-hero-wrapper row">
@@ -59,54 +27,7 @@ const Home = () => (
     {/* HERO ENDS */}
     <div className="container">
       {/* HOME PAGE STARTS */}
-      <section className="section-title">
-        <div className="px-2">
-          <div className="pt-5 pb-4">
-            <h1>Portfolios</h1>
-          </div>
-        </div>
-      </section>
-      <section className="pb-5">
-        <div className="row">
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <a href="" className="btn btn-main bg-blue ttu">See More Portfolios</a>
+      
       <section className="section-title">
         <div className="px-2">
           <div className="pt-5 pb-4">
@@ -154,74 +75,7 @@ const Home = () => (
       <a href="" className="btn btn-main bg-blue ttu">See More Posts</a>
       {/* HOME PAGE ENDS */}
       {/* FORUM CATEGORIES START */}
-      <section className="section-title">
-        <div className="px-2">
-          <div className="pt-5 pb-4">
-            <h1>Categories</h1>
-          </div>
-        </div>
-      </section>
-      <section className="fj-category-list">
-        <div className="row">
-          <div className="col-md-4">
-            <div className="fj-category-container">
-              <a className="fj-category subtle-shadow no-border" href="#">
-                {
-                // <div className="category-icon">
-                //   <img src="images/pen.png" />
-                // </div>
-                }
-                <div className="category-information">
-                  <div className="heading gray-90">
-                    General Discussion
-                  </div>
-                  <div className="description">
-                    Just general question
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="fj-category-container">
-              <a className="fj-category subtle-shadow no-border" href="#">
-                {
-                // <div className="category-icon">
-                //   <img src="images/pen.png" />
-                // </div>
-                }
-                <div className="category-information">
-                  <div className="heading gray-90">
-                    Other Discussion
-                  </div>
-                  <div className="description">
-                    Just general question
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="fj-category-container">
-              <a className="fj-category subtle-shadow no-border" href="#">
-                {
-                // <div className="category-icon">
-                //   <img src="images/pen.png" />
-                // </div>
-                }
-                <div className="category-information">
-                  <div className="heading gray-90">
-                    Some Discussion
-                  </div>
-                  <div className="description">
-                    Just general question
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
+      
       {/* FORUM CATEGORIES ENDS */}
       {/* TOPICS PAGE STARTS */}
       <section className="section-title">
