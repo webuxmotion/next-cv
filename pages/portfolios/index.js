10da4317b6cf610071bc97517c0d516842e72eca
Andrii Pereverziev <andriipereverziev@Andriis-MacBook-Pro.local>
2020-11-13 19:46:57 +0200

    Create portfolio from client

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import axios from 'axios';
 import PortfolioCard from '@/components/portfolios/Card';
 import Link from 'next/link';
@@ -20,7 +21,46 @@ const fetchPortfolios = () => {
     .then(res => res.data.data.portfolios)
 }
 
-const Portfolios = ({ portfolios }) => {
+const handleCreatePortrolio = () => {
+  const query = `
+    mutation CreatePortfolio {
+      createPortfolio (input: {
+        title: "New from client"
+        company: "new company"
+        companyWebsite: "new.com"
+        location: "Spain"
+        jobTitle: "Front end engineer"
+        description: "Work, work"
+        startDate: "01/01/2014"
+        endDate: "01/01/2016"
+      }) {
+        _id,
+        title,
+        company,
+        companyWebsite
+        location
+        jobTitle
+        description
+        startDate
+        endDate
+      }
+    }
+  `;
+
+  return axios.post('http://localhost:3000/graphql', { query })
+    .then(({ data: graph }) => graph.data)
+    .then(data => data.createPortfolio)
+}
+
+const Portfolios = ({ data }) => {
+  const [portfolios, setPortfolios] = useState(data?.portfolios);
+
+  const createPortfolio = async () => {
+    const newPortfolio = await handleCreatePortrolio();
+    const newPortfolios = [...portfolios, newPortfolio];
+
+    setPortfolios(newPortfolios);
+  }
 
   return (
     <>
@@ -30,6 +70,7 @@ const Portfolios = ({ portfolios }) => {
             <h1>Portfolios</h1>
           </div>
         </div>
+        <button onClick={createPortfolio} className="btn btn-primary">Create portfolio</button>
       </section>
       
       <section className="pb-5">
@@ -59,7 +100,7 @@ const Portfolios = ({ portfolios }) => {
 Portfolios.getInitialProps = async () => {
   const portfolios = await fetchPortfolios();
 
-  return { portfolios }
+  return { data: { portfolios } }
 }
 
 export default Portfolios;
