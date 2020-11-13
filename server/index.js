d475d9bfe61e31d9100d4be5cd951f50ec3bcd00
Andrii Pereverziev <andriipereverziev@Andriis-MacBook-Pro.local>
2020-11-13 15:56:07 +0200

    Data rework. Use nodemon

--- server/index.js
+++ server/index.js
@@ -8,14 +8,65 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const data = {
+  portfolios: [
+    {
+      _id: "sad87da79",
+      title: 'Job in Netcentric',
+      company: 'Netcentric',
+      companyWebsite: 'www.google.com',
+      location: 'Spain, Barcelona',
+      jobTitle: 'Engineer',
+      description: 'Doing something, programing....',
+      startDate: '01/01/2014',
+      endDate: '01/01/2016'
+    },
+    {
+      _id: "da789ad1",
+      title: 'Job in Siemens',
+      company: 'Siemens',
+      companyWebsite: 'www.google.com',
+      location: 'Slovakia, Kosice',
+      jobTitle: 'Software Engineer',
+      description: 'Responsoble for parsing framework for JSON medical data.',
+      startDate: '01/01/2011',
+      endDate: '01/01/2013'
+    },
+    {
+      _id: "sadcxv9",
+      title: 'Work in USA',
+      company: 'WhoKnows',
+      companyWebsite: 'www.google.com',
+      location: 'USA, Montana',
+      jobTitle: 'Housekeeping',
+      description: 'So much responsibility....Overloaaaaaad',
+      startDate: '01/01/2010',
+      endDate: '01/01/2011'
+    }
+  ]
+}
+
 app.prepare().then(() => {
   const server = express()
 
-
   // Construct a schema
   const schema = buildSchema(`
+    type Portfolio {
+      _id: ID
+      title: String
+      company: String
+      companyWebsite: String
+      location: String
+      jobTitle: String
+      description: String
+      startDate: String
+      endDate: String
+    }
+
     type Query {
       hello: String
+      portfolio: Portfolio
+      portfolios: [Portfolio]
     }
   `);
 
@@ -23,6 +74,12 @@ app.prepare().then(() => {
   const root = {
     hello: () => {
       return 'Hello World!'
+    },
+    portfolio: () => {
+      return data.portfolios[0]
+    },
+    portfolios: () => {
+      return data.portfolios
     }
   }
 
