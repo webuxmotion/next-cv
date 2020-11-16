97b574d0a40fa1b68d1fc4a4123229f8b2cdbe75
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-11-16 12:05:43 +0200

    Client. Register user and redirect to login

--- pages/login.js
+++ pages/login.js
@@ -2,14 +2,7 @@ const Login = () => {
 
   return (
     <>
-      <section className="section-title">
-        <div className="px-2">
-          <div className="pt-5 pb-4">
-            <h1>Login Page</h1>
-          </div>
-        </div>
-      </section>
-      <div className="bwm-form">
+      <div className="bwm-form mt-5">
         <div className="row">
           <div className="col-md-5 mx-auto">
             <h1 className="page-title">Login</h1>
--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,36 @@
-const Register = () => (
-  <div>Register</div>
-)
+import RegisterForm from '@/components/forms/RegisterForm';
+import { Mutation } from 'react-apollo';
 
-export default Register;
+import { SIGN_UP } from '@/apollo/queries';
+import withApollo from '@/hoc/withApollo';
+import Redirect from '@/components/shared/Redirect';
+
+const Register = () => {
+
+  return (
+    <>
+      <div className="bwm-form mt-5">
+        <div className="row">
+          <div className="col-md-5 mx-auto">
+            <h1 className="page-title">Register</h1>
+            <Mutation mutation={SIGN_UP}>
+              { 
+                (signUpUser, { data, error }) => 
+                  <>
+                    <RegisterForm onSubmit={registerData => {
+                      signUpUser({ variables: registerData })
+                    }} />
+                    { 
+                      data && data.signUp && <Redirect to="/login" />
+                    }
+                  </>
+              }
+            </Mutation>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default withApollo(Register);
