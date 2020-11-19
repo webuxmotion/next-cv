9f413547d11e2db16b81a7a3323a2706615e0fee
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-11-19 22:13:48 +0200

    CV

--- pages/cv.js
+++ pages/cv.js
@@ -1,5 +1,20 @@
-const Cv = () => (
-  <div>CV</div>
-)
+import BaseLayout from '@/layouts/BaseLayout';
+
+const Cv = () => 
+  <BaseLayout>
+    <div className="row mt-4">
+      <div className="col-md-8 offset-md-2">
+        <iframe 
+          style={{
+            width: '100%',
+            height: '800px'
+          }}
+          src="/cv.pdf"
+        >
+
+        </iframe>
+      </div>
+    </div>
+  </BaseLayout>
 
 export default Cv;
